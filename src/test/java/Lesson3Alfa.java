0a987e9ee94f071ee6d02c3c14be11f64826d0cd
z000mg <zooomg2015@yandex.ru>
2020-11-19 11:37:54 +0300

    start

--- src/test/java/Lesson3Alfa.java
+++ src/test/java/Lesson3Alfa.java
@@ -1,2 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class Lesson3Alfa {
+
+    @BeforeAll
+    static void setup(){
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void lesson3Alfa() {
+        open("https://alfabank.ru/make-money/");
+        $("#filter");
+        $(By.linkText("Депозиты");
+
+        open("https://alfabank.ru/make-money/");
+    }
 }
