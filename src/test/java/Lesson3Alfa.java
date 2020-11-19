f407ceebc24eaf528a2fc23e5f772f3292b59a30
z000mg <zooomg2015@yandex.ru>
2020-11-19 17:42:28 +0300

    final

--- src/test/java/Lesson3Alfa.java
+++ src/test/java/Lesson3Alfa.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,9 +17,13 @@ public class Lesson3Alfa {
 
     @Test
     void lesson3Alfa() {
+        //На страничке "Вклады" (https://alfabank.ru/make-money/) перейти на подстраничку "Депозиты" (https://alfabank.ru/make-money/deposits/)
         open("https://alfabank.ru/make-money/");
         $("#filter").$("div").$("div").$(byText("Депозиты")).parent().click();
-1
-        open("https://alfabank.ru/make-money/");
+
+        //- Перейти по линку на "Архивные депозиты" (https://alfabank.ru/make-money/archive/)
+        $("#more-buttons").$("div").$("a").$(byText("Архивные счета и депозиты")).parent().click();
+        $("[data-test-id=tabs-list-tabTitle-1]").$$(byText("Депозиты")).find(Condition.visible).click();
+        $("#filter").$$("[data-widget-name=CatalogCard]").shouldHaveSize(5);
     }
 }
