79988048ce6edf0a1fcc2bdd01ba6b595aff8c50
Z000mg <zooomg2015@yandex.ru>
2020-11-22 21:12:13 +0300

    final

--- src/test/java/Lesson3Alfa.java
+++ src/test/java/Lesson3Alfa.java
@@ -18,19 +18,10 @@ public class Lesson3Alfa {
     @Test
     void lesson3Alfa() {
 
-        //На страничке "Вклады" (https://alfabank.ru/make-money/) перейти на подстраничку "Депозиты" (https://alfabank.ru/make-money/deposits/)
         open("https://alfabank.ru/make-money/");
         $("#filter").$("div").$("div").$(byText("Депозиты")).parent().click();
-
-        //- Перейти по линку на "Архивные депозиты" (https://alfabank.ru/make-money/archive/)
         $("#more-buttons").$("div").$("a").$(byText("Архивные счета и депозиты")).parent().click();
         $("[data-test-id=tabs-list-tabTitle-1]").$$(byText("Депозиты")).find(Condition.visible).click();
         $("#filter").$$("[data-widget-name=CatalogCard]").shouldHaveSize(5);
-
-        open("https://alfabank.ru/make-money/");
-        $("[data-test-id=tabs-list-tabTitle-0]").sibling(0).click();
-        $(byText("Альфа-Банк является участником системы обязательного страхования вкладов")).shouldBe(Condition.visible);
-        $("[data-test-id=tabs-list-tabTitle-0]").sibling(1).click();
-        $(byText("Что такое вклад?")).shouldBe(Condition.visible);
         }
 }
