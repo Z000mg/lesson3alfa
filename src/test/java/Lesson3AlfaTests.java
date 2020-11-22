c19b5cd63f6a355fd9300bb8280e58d6cda6b93b
Z000mg <zooomg2015@yandex.ru>
2020-11-22 22:54:24 +0300

    final

--- src/test/java/Lesson3AlfaTests.java
+++ src/test/java/Lesson3AlfaTests.java
@@ -1,8 +1,9 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,19 +16,19 @@ public class Lesson3AlfaTests {
     }
 
     @Test
-    void depositesTest() {
+    void depositsTest() {
         open("https://alfabank.ru/make-money/");
         $("#filter").$("div").$("div").$(byText("Депозиты")).parent().click();
         $("#more-buttons").$("div").$("a").$(byText("Архивные счета и депозиты")).parent().click();
-        $("[data-test-id=tabs-list-tabTitle-1]").$$(byText("Депозиты")).find(Condition.visible).click();
+        $("[data-test-id=tabs-list-tabTitle-1]").$$(byText("Депозиты")).find(visible).click();
         $("#filter").$$("[data-widget-name=CatalogCard]").shouldHaveSize(5);
     }
 
     @Test
-    void vkladStrahTest () {
+    void insuranceDeposits () {
         open("https://alfabank.ru/make-money/");
-        $(byText("Описание")).parent().sibling(1).shouldHave(Condition.text("Что такое вклад?"));
-        $(byText("Что такое вклад?")).parent().preceding(0).shouldHave(Condition.text("Страхование вкладов"));
-        $(byText("Что такое вклад?")).closest("button").preceding(0).shouldHave(Condition.text("Страхование вкладов"));
+        $(byText("Описание")).parent().sibling(1).shouldHave(text("Что такое вклад?"));
+        $(byText("Что такое вклад?")).parent().preceding(0).shouldHave(text("Страхование вкладов"));
+        $(byText("Что такое вклад?")).closest("button").preceding(0).shouldHave(text("Страхование вкладов"));
     }
 }
