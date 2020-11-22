ff102f413a67d8b92510fc2cf813624fced3f151
Z000mg <zooomg2015@yandex.ru>
2020-11-22 21:37:43 +0300

    final

--- src/test/java/Lesson3Alfa.java
+++ src/test/java/Lesson3Alfa.java
@@ -16,12 +16,20 @@ public class Lesson3Alfa {
     }
 
     @Test
-    void lesson3Alfa() {
+    void depositesTest() {
 
         open("https://alfabank.ru/make-money/");
         $("#filter").$("div").$("div").$(byText("Депозиты")).parent().click();
         $("#more-buttons").$("div").$("a").$(byText("Архивные счета и депозиты")).parent().click();
         $("[data-test-id=tabs-list-tabTitle-1]").$$(byText("Депозиты")).find(Condition.visible).click();
         $("#filter").$$("[data-widget-name=CatalogCard]").shouldHaveSize(5);
-        }
+    }
+
+    @Test
+    void vkladStrahTest () {
+        open("https://alfabank.ru/make-money/");
+        $(byText("Описание")).parent().sibling(1).shouldHave(Condition.text("Что такое вклад?"));
+        $(byText("Что такое вклад?")).parent().preceding(0).shouldHave(Condition.text("Страхование вкладов"));
+        $(byText("Что такое вклад?")).closest("button").preceding(0).shouldHave(Condition.text("Страхование вкладов"));
+    }
 }
